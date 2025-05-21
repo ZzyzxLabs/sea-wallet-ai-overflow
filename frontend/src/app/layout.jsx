8db5977044c6ec2247743f0290660830ce78b140
Fu <frederic010992@gmail.com>
2025-05-22 00:15:05 +0800

    add metadata

--- frontend/src/app/layout.jsx
+++ frontend/src/app/layout.jsx
@@ -18,19 +18,36 @@ const inter = Inter({
 });
 
 export const metadata = {
-  title: "Smart Will",
-  description: ":3",
+  title: "SeaWallet.ai",
+  description: "Your secure and intelligent crypto wallet.",
+  keywords: ["crypto", "wallet", "blockchain", "SeaWallet", "AI", "web3"],
+  authors: [{ name: "Zzyzx Labs", url: "https://seawallet.ai" }],
+  themeColor: "#4da2ff",
+  openGraph: {
+    title: "SeaWallet.ai",
+    description: "Your secure and intelligent crypto wallet.",
+    url: "https://seawallet.ai",
+    siteName: "SeaWallet.ai",
+    images: [
+      {
+        url: "/icon.png",
+        width: 1200,
+        height: 630,
+        alt: "SeaWallet.ai Open Graph Image",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${inter.variable} antialiased`}
       >
-        <Providers>
-          {children}
-        </Providers>
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
