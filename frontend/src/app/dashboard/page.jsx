1ecaeae3f740e3254d2b1ced5a30b1a48d8788df
creaper9487 <58578328+creaper9487@users.noreply.github.com>
2025-06-12 23:59:56 +0800

    feat: fix layout by properly closing div in Dashboard component

--- frontend/src/app/dashboard/page.jsx
+++ frontend/src/app/dashboard/page.jsx
@@ -7,8 +7,10 @@ export default function Dashboard() {
     <div className="min-h-screen flex flex-col">
 
       <div className="flex flex-col items-center justify-center flex-1 bg-primary/10 relative overflow-hidden">
+      <div className="w-2/3 rounded-2xl bg-white/45 p-12">
         <h1 className="text-4xl font-bold mb-4 relative z-10">Vault Content</h1>
         <VaultList />
+      </div>
       </div>
     </div>
   );
